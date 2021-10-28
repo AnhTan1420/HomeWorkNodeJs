eac7615df3a58f1a34b3b4cfcd758d4fc560ff4c
AnhTan1420 <levananhtan.it19a2b@gmail.com>
2021-10-28 16:13:20 +0700

    kiemtra

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,7 @@
+const { required } = require('joi');
+
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.studentController = require('./student.controllers');
 module.exports.classController = require("./class.controller");
+module.exports.detaiController = require("./detai.controller");
---
+++ src/models/Detai_levananhtan_22.js
@@ -0,0 +1,41 @@
+const { boolean } = require('joi');
+const mongoose = require('mongoose');
+const { input } = require('../config/logger');
+const { toJSON, paginate } = require('./plugins');
+
+const detaiSchema = mongoose.Schema(
+  {
+    MaDT: {
+      type: String,
+      required: true,
+      trim: true,
+      char:20,
+    },
+    TenDT: {
+      type: String,
+      required: true,
+      trim: true,
+      char: 30,
+    },
+    KinhPhi: {
+      type: input,
+    },
+    NoiThuctap: {
+        type: String,
+        required: true,
+        trim: true,
+        char: 30,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+studentSchema.plugin(toJSON);
+studentSchema.plugin(paginate);
+
+const Detai = mongoose.model('Student', detaiSchema);
+
+module.exports = Detai;
